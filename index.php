e2fad6294219be1da7502cc4d7be1c6815c5e1ce
Francisco_Ripardo_NUVIO <francisco.ripardo@nuvio.com.br>
2025-02-15 09:22:02 -0400

    add cors related headers + removed repository name from conditions in router

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
-require_once "./src/router/router.php";
+header("Access-Control-Allow-Origin: *");  // Permite qualquer origem
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
 
-$uri = $_SERVER['REQUEST_URI'];
+require_once "./src/router/router.php";
+// $localIP = getHostByName(getHostName());
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = Router::clean_uri($uri);
 $receivedData = file_get_contents("php://input");
